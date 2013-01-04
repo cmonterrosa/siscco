47262df5fb7368178ef240184ef071afd283a298
Carlos Monterrosa <cmonterrosa@gmail.com>
2013-01-04 09:52:03 -0600

    + Agregamos metodo para mostrar nombre completo de promotor

--- app/models/promotor.rb
+++ app/models/promotor.rb
@@ -5,6 +5,10 @@ class Promotor < ActiveRecord::Base
   def nombre_completo
     "#{paterno} #{materno} #{nombre}"
   end
+
+  def nombre_completo_desc
+    "#{nombre} #{paterno} #{materno}"
+  end
   #validates_format_of :email, :with =>/\A([^@\s]+)@((?:[-a-z0-9]+.)+[a-z]{2,})\Z/i, :message => "Campo Email no válido"
 
 end
