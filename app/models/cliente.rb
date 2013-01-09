8b179a4e62944d7140dfb2c75be117f56beca31f
Carlos Monterrosa <cmonterrosa@gmail.com>
2013-01-09 08:45:07 -0600

    + Curp uniquesess restriction

--- app/models/cliente.rb
+++ app/models/cliente.rb
@@ -29,7 +29,7 @@ class Cliente < ActiveRecord::Base
 #------- Validaciones -----------
 #validates_uniqueness_of :rfc, :message => ", Ese cliente ya esta registrado."
 #validates_uniqueness_of :identificador, :message => ", Ese cliente ya esta registrado."
-#validates_uniqueness_of :curp, :message => ", Ese cliente ya esta registrado."
+validates_uniqueness_of :curp, :message => ", Ese cliente ya esta registrado."
 validates_length_of :rfc, :in => 10..13,  :message => ", Longitud incorrecta"
 validates_length_of :curp, :is => 18,  :message => ", Longitud incorrecta"
 #validates_associated :grupos, :message => "existen registros en otras tablas"
