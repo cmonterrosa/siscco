6265d4047aec9175a658618e8b84573578e3fd08
Carlos Monterrosa <cmonterrosa@gmail.com>
2013-01-18 12:49:59 -0600

    + full description method for show a name and id

--- app/models/grupo.rb
+++ app/models/grupo.rb
@@ -4,5 +4,19 @@ class Grupo < ActiveRecord::Base
   has_many :creditos
   #---- relacion ficticia -----
    has_many :clientegrupos
-   validates_uniqueness_of :nombre, :message => ", Ese grupo ya esta registrado."
+   validates_uniqueness_of :identificador, :scope => "nombre", :message => ", Ese grupo y propuesta estan registradas."
+
+
+#  def before_save
+#        id = Array.new(2) { (rand(122-97) + 97).chr }.join + (rand(10000)).to_s
+#        while not (Grupo.find_by_identificador(id)).nil?
+#            id = Array.new(2) { (rand(122-97) + 97).chr }.join + (rand(10000)).to_s
+#        end
+#        self.identificador = id.to_s.ljust(6, "0")
+#  end
+
+  def full_description
+    "#{self.identificador} | #{self.nombre}"
+  end
+
 end
