8ecdf2b4d9aa5b90bf29982ef84a8a2cb3e73e41
Carlos Monterrosa <cmonterrosa@gmail.com>
2013-04-18 19:49:37 -0500

    + Modificacion para mostrar solo 1000 registros sin grupo y optimizar busqueda dinamica

--- lib/creditos.rb
+++ lib/creditos.rb
@@ -453,7 +453,8 @@ module Creditos
 
 
          def todos_clientes_singrupo_join
-            return Cliente.find_by_sql("select id, curp, paterno, materno, nombre from clientes where id not in (select cliente_id from clientes_grupos where activo=1) order by paterno, materno, nombre")
+           #return Cliente.find_by_sql("select id, curp, paterno, materno, nombre from clientes where id not in (select cliente_id from clientes_grupos where activo=1) order by paterno, materno, nombre")
+            return Cliente.find_by_sql("select c.id, c.curp, c.paterno, c.materno, c.nombre from clientes c left join clientes_grupos cg on c.id=cg.cliente_id where (cg.activo IS NULL  OR cg.activo = 0) LIMIT 1000")
          end
 
          def grupo_activo_cliente(cliente)
