072e9439e102ac112f971793f04ccc758a8564c2
Carlos Monterrosa <cmonterrosa@gmail.com>
2013-04-26 15:17:50 -0500

    + New index and role model

---
+++ db/migrate/20130426185624_add_index_clientegrupo.rb
@@ -0,0 +1,9 @@
+class AddIndexClientegrupo < ActiveRecord::Migration
+def self.up
+     add_index "clientes_grupos", "activo"
+  end
+
+  def self.down
+     remove_index "clientes_grupos", "activo"
+  end
+end
