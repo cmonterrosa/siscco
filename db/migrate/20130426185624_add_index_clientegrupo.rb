9aa1a28e746cca7209c95127f5e8c22b7ebcd707
Rafael Hernandez Escobar <snakheertax@gmail.com>
2013-05-07 16:27:21 -0500

    Renombrar migrate

--- db/migrate/20130426185624_add_index_clientegrupo.rb
+++
@@ -1,9 +0,0 @@
-class AddIndexClientegrupo < ActiveRecord::Migration
-def self.up
-     add_index "clientes_grupos", "activo"
-  end
-
-  def self.down
-     remove_index "clientes_grupos", "activo"
-  end
-end
