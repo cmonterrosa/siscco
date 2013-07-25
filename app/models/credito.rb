3d36e44407f40971d2b24cac1f5e975ad421b2f1
Carlos Monterrosa <cmonterrosa@gmail.com>
2013-07-24 20:16:36 -0500

    + Modificaciones para ir adecuando nuevo algoritmo de creacion de creditos

--- app/models/credito.rb
+++ app/models/credito.rb
@@ -142,6 +142,209 @@ class Credito < ActiveRecord::Base
     @capital = credito.monto.to_f
     @capital_semanal = @capital_total / @producto.num_pagos
     @cliente = Cliente.find(self.cliente_id) if self.cliente_id
+    if @tipo == "GRUPAL"
+        case tipos_interes
+            when "SALDOS INSOLUTOS (SSI)"
+              @tasa_semanal = round((((@producto.tasa_anualizada.to_f) / 360.0 ) * 7) / 100.0, 4)
+              @pago_semanal = round((@capital_grupal * (@tasa_semanal/(1-(1 + @tasa_semanal)**(@producto.num_pagos*-1)))),2)
+                  clientes_activos_grupo(Grupo.find(credito.grupo_id)).each do |y|
+                  contador=1
+                  saldo_inicial = @capital_grupal
+                  arreglo_pagos.each do |x|
+                      @interes_minimo = round(saldo_inicial * @tasa_semanal, 2)
+                      @principal_recuperado = @pago_semanal - @interes_minimo
+                      Pago.create(:num_pago => contador,
+                                 :credito_id => credito.id,
+                                 :fecha_limite => x,
+                                 :cliente_id => y.id.to_i,
+                                 :capital_minimo => @principal_recuperado,
+                                 :principal_recuperado => @principal_recuperado,
+                                 :interes_minimo => @interes_minimo,
+                                :saldo_inicial => saldo_inicial,
+                                :saldo_final => (saldo_inicial - @principal_recuperado),
+                                :pagado => 0,
+                                :descripcion => tipos_interes)
+                      contador+=1
+                      saldo_inicial -= @principal_recuperado
+                end
+              end
+
+            when "SALDOS GLOBALES"
+                  @capital = @credito.monto
+                  saldo_inicial = @capital
+                  contador=1
+                  @meses = @credito.producto.num_pagos / 4
+                  @pagos_semanales = @credito.producto.num_pagos
+                  @pago_semanal_sin_iva = ((((@credito.producto.tasa_mensual_ssg / 100) * @meses) * @capital) + @capital) / @pagos_semanales
+                  @pago_capital_semanal = (@capital / @pagos_semanales )
+                  @pago_interes_semanal = @pago_semanal_sin_iva - @pago_capital_semanal
+                  @iva_semanal = @pago_interes_semanal * (@credito.producto.iva / 100)
+                  @pago_total_semanal = @pago_capital_semanal + @pago_interes_semanal + @iva_semanal
+                  @interes_global =  @pago_interes_semanal * @pagos_semanales
+                  @iva_global = @iva_semanal * @pagos_semanales
+                  @credito.interes_global = @interes_global if @interes_global
+                  @credito.iva_global = @iva_global if @iva_global
+                  #--- Hacemos una iteracion por todos los miembros del grupo y dividimos el total del credito ---
+                  @capital = (credito.monto.to_f / credito.grupo.clientes.size.to_f)
+                  @capital_semanal = @capital / @producto.num_pagos
+
+                  #### Creamos registros padres #######
+                  arreglo_pagos.each do |x|
+                       Pagogrupal.create(:fecha_limite => x,
+                              :capital_minimo => @pago_capital_semanal,
+                              :interes_minimo => @pago_interes_semanal,
+                              :pagado => false,
+                              :credito_id => @credito.id,
+                              :saldo_inicial => saldo_inicial,
+                              :saldo_final => saldo_inicial - @pago_capital_semanal,
+                              :principal_recuperado => @pago_capital_semanal,
+                              :num_pago => contador,
+                              :iva => @iva_semanal)
+                              saldo_inicial -= @capital_semanal
+                    end
+
+                    clientes_activos_grupo(Grupo.find(credito.grupo_id)).each do |y|
+                    contador=1
+                    saldo_inicial = @capital
+                    @total_capital = @capital
+                    @meses = @credito.producto.num_pagos / 4
+                    @pagos_semanales = @credito.producto.num_pagos
+                    #PAGO SEMANAL SIN INVA     (((tasa_mensual_ssg * meses) * monto) + monto) / pagos_semanales
+                    @pago_semanal_sin_iva = ((((@credito.producto.tasa_mensual_ssg / 100) * @meses) * @capital) + @capital) / @pagos_semanales
+                    @pago_capital_semanal = (@capital / @pagos_semanales )
+                    @pago_interes_semanal = @pago_semanal_sin_iva - @pago_capital_semanal
+                    @iva_semanal = @pago_interes_semanal * (@credito.producto.iva / 100)
+                    @pago_total_semanal = @pago_capital_semanal + @pago_interes_semanal + @iva_semanal
+
+                    ####### CREAMOS REGISTROS GENERAL ######
+                    
+                    arreglo_pagos.each do |x|
+                      Pago.create(:num_pago => contador,
+                                 :credito_id => credito.id,
+                                 :fecha_limite => x,
+                                 :cliente_id => y.id.to_i,
+                                 :capital_minimo => @pago_capital_semanal,
+                                 :iva => @iva_semanal,
+                                 :principal_recuperado => @capital_semanal,
+                                 :interes_minimo => @pago_interes_semanal,
+                                 :saldo_inicial => saldo_inicial,
+                                 :saldo_final => saldo_inicial - @capital_semanal,
+                                 :pagado => 0,
+                                 :descripcion => tipos_interes)
+                          contador+=1
+                      saldo_inicial -= @capital_semanal
+                    end
+                    ## UPDATE CREDITO INFO
+                    @credito.save
+                 end
+
+      end
+
+
+    else
+
+     ##########################################################################
+     ############### CREDITOS INDIVIDUALES ####################################
+     ##########################################################################
+
+      case tipos_interes
+        when "SALDOS INSOLUTOS (SSI)"
+            @pago_semanal = round((@capital * (@tasa_semanal/(1-(1 + @tasa_semanal)**(@producto.num_pagos*-1)))),2)
+            contador=1
+            saldo_inicial = @capital
+            arreglo_pagos.each do |x|
+                    @interes_minimo = round(saldo_inicial * @tasa_semanal, 2)
+                    @principal_recuperado = @pago_semanal - @interes_minimo
+                    Pago.create(:num_pago => contador,
+                               :credito_id => credito.id,
+                               :fecha_limite => x,
+                               :cliente_id => @cliente.id,
+                               :capital_minimo => @principal_recuperado,
+                               :principal_recuperado => @principal_recuperado,
+                              :interes_minimo => @interes_minimo,
+                              :saldo_inicial => saldo_inicial,
+                              :saldo_final => (saldo_inicial - @principal_recuperado),
+                              :pagado => 0,
+                              :descripcion => tipos_interes)
+                    contador+=1
+                    saldo_inicial -= @principal_recuperado
+              end
+
+        when "SALDOS GLOBALES"
+                  @capital = @credito.monto
+                  saldo_inicial = @capital
+                  contador=1
+                  @meses = @credito.producto.num_pagos / 4
+                  @pagos_semanales = @credito.producto.num_pagos
+                  @pago_semanal_sin_iva = ((((@credito.producto.tasa_mensual_ssg / 100) * @meses) * @capital) + @capital) / @pagos_semanales
+                  @pago_capital_semanal = (@capital / @pagos_semanales )
+                  @pago_interes_semanal = @pago_semanal_sin_iva - @pago_capital_semanal
+                  @iva_semanal = @pago_interes_semanal * (@credito.producto.iva / 100)
+                  @pago_total_semanal = @pago_capital_semanal + @pago_interes_semanal + @iva_semanal
+                  @interes_global =  @pago_interes_semanal * @pagos_semanales
+                  @iva_global = @iva_semanal * @pagos_semanales
+                  @credito.interes_global = @interes_global if @interes_global
+                  @credito.iva_global = @iva_global if @iva_global
+                  #--- Hacemos una iteracion por todos los miembros del grupo y dividimos el total del credito ---
+                  @capital = (credito.monto.to_f / credito.grupo.clientes.size.to_f)
+                  @capital_semanal = @capital / @producto.num_pagos
+                  #### Creamos registros padres #######
+                  arreglo_pagos.each do |x|
+                        Pagogrupal.create(:fecha_limite => x,
+                              :capital_minimo => @pago_capital_semanal,
+                              :interes_minimo => @pago_interes_semanal,
+                              :pagado => false,
+                              :credito_id => @credito.id,
+                              :saldo_inicial => saldo_inicial,
+                              :saldo_final => saldo_inicial - @pago_capital_semanal,
+                              :principal_recuperado => @pago_capital_semanal,
+                              :num_pago => contador,
+                              :iva => @iva_semanal)
+                              saldo_inicial -= @capital_semanal
+                    end
+
+                    ####### Iteracion por cada uno de los clientes ################
+                    @cliente = Cliente.find(self.cliente_id)
+                    contador=1
+                    saldo_inicial = @capital
+                    @total_capital = @capital
+                    @meses = @credito.producto.num_pagos / 4
+                    @pagos_semanales = @credito.producto.num_pagos
+                    #PAGO SEMANAL SIN INVA     (((tasa_mensual_ssg * meses) * monto) + monto) / pagos_semanales
+                    @pago_semanal_sin_iva = ((((@credito.producto.tasa_mensual_ssg / 100) * @meses) * @capital) + @capital) / @pagos_semanales
+                    @pago_capital_semanal = (@capital / @pagos_semanales )
+                    @pago_interes_semanal = @pago_semanal_sin_iva - @pago_capital_semanal
+                    @iva_semanal = @pago_interes_semanal * (@credito.producto.iva / 100)
+                    @pago_total_semanal = @pago_capital_semanal + @pago_interes_semanal + @iva_semanal
+
+                    ####### CREAMOS REGISTROS INDIVIDUALES ######
+                    arreglo_pagos.each do |x|
+                      Pago.create(:num_pago => contador,
+                                 :credito_id => credito.id,
+                                 :fecha_limite => x,
+                                 :cliente_id => @cliente.id.to_i,
+                                 :capital_minimo => @pago_capital_semanal,
+                                 :iva => @iva_semanal,
+                                 :principal_recuperado => @capital_semanal,
+                                 :interes_minimo => @pago_interes_semanal,
+                                 :saldo_inicial => saldo_inicial,
+                                 :saldo_final => saldo_inicial - @capital_semanal,
+                                 :pagado => 0,
+                                 :descripcion => tipos_interes)
+                          contador+=1
+                      saldo_inicial -= @capital_semanal
+                    end
+                    ## UPDATE CREDITO INFO
+                    @credito.save
+                 
+        
+         end ### ciera case
+    end
+
+
+
+
+
     case tipos_interes
         when "SALDOS INSOLUTOS (SSI)"
           @tasa_semanal = round((((@producto.tasa_anualizada.to_f) / 360.0 ) * 7) / 100.0, 4)
